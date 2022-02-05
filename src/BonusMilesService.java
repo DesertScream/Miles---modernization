33c5fe5b602fd9252c31473e532ef11f3c4b4ffa
DesertScream <www.anton.dzuin@gmail.com>
2022-02-05 23:17:02 +0500

    new code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class BonusMilesService {
+    public int calculate(int ticketPrice) {
+        Scanner x = new Scanner(System.in);
+        int price = x.nextInt();
+        int miles = 20;
+        int AccruedMiles = price / miles;
+        return AccruedMiles;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        int ticketPrice = x.nextInt();
-        Scanner y = new Scanner(System.in);
-        int amountOfRublesPerOneBonusMile = y.nextInt();
-        int numberOfAccruedMiles = ticketPrice / amountOfRublesPerOneBonusMile;
-        System.out.println(numberOfAccruedMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+
     }
 }
